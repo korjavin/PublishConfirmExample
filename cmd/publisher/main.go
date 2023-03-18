00b557c21f359d363af26be4b508bcbe1848370e
Korzhavin Ivan <ikorjavin@3pm.ai>
2023-03-18 20:40:41 +0300

    Add saga

--- cmd/publisher/main.go
+++ cmd/publisher/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"time"
 
+	"github.com/itimofeev/go-saga"
 	_ "github.com/lib/pq"
 	"github.com/streadway/amqp"
 )
@@ -68,43 +70,67 @@ func main() {
 		if err := rows.Scan(&id); err != nil {
 			log.Fatal(err)
 		}
+		message := fmt.Sprintf("Message %d", id)
+		// Create a new saga
+		s := saga.NewSaga("Sending message")
 
-		err := ch.Tx()
+		// Add a step to update the is_sent flag in PostgreSQL
+		s.AddStep(&saga.Step{
+			Name: "Update is_sent flag",
+			Func: func(ctx context.Context) error {
+				return updateMessageAsSent(db, id)
+			},
+			CompensateFunc: func(ctx context.Context) error {
+				return updateMessageAsNotSent(db, id)
+			},
+		})
+
+		// Add a step to send the message to RabbitMQ
+		s.AddStep(&saga.Step{
+			Name: "Send message to RabbitMQ",
+			Func: func(ctx context.Context) error {
+				if id > 2 {
+					log.Printf("I will sleep for 30 sec, please find a way to kill postgresql or/and rabbitmq to see if messages to rmq will be aborted as well")
+					time.Sleep(30 * time.Second)
+				}
+				log.Printf("Sending message: %s to queue %s", message, q.Name)
+				return publishMessage(ch, q.Name, message)
+			},
+			CompensateFunc: func(ctx context.Context) error { return nil },
+		})
+
+		// Execute the saga
+		store := saga.New()
+		c := saga.NewCoordinator(context.Background(), context.Background(), s, store)
+		c.Play()
 		if err != nil {
-			log.Fatalf("Failed to begin a transaction: %v", err)
+			log.Printf("Saga execution failed for message ID %d: %v", id, err)
+		} else {
+			log.Printf("Saga execution succeeded for message ID %d", id)
 		}
-
-		// Publish the message to the RabbitMQ queue
-		log.Printf("Sending message: %d to queue %s", id, q.Name)
-		err = ch.Publish(
-			"",     // exchange
-			q.Name, // routing key
-			true,   // mandatory
-			false,  // immediate
-			amqp.Publishing{
-				ContentType:  "text/plain",
-				Body:         []byte(fmt.Sprintf("%d", id)),
-				DeliveryMode: amqp.Persistent,
-			})
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if id > 2 {
-			log.Printf("I will sleep for 30 sec, please find a way to kill postgresql to see if messages to rmq will be aborted as well")
-			time.Sleep(30 * time.Second)
-		}
-
-		// Set the "is_sent" flag to true for the current id in the database
-		_, err = db.Exec("UPDATE messages SET is_sent = true WHERE id = $1", id)
-		if err != nil {
-			ch.TxRollback()
-			log.Fatal(err)
-		}
-		ch.TxCommit()
 	}
 
 	if err := rows.Err(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func publishMessage(ch *amqp.Channel, queueName string, message string) error {
+	return ch.Publish("", queueName, true, false, amqp.Publishing{
+		ContentType:  "text/plain",
+		Body:         []byte(message),
+		DeliveryMode: amqp.Persistent,
+	})
+}
+
+func updateMessageAsSent(db *sql.DB, id int) error {
+	log.Printf("Updating message %d as sent", id)
+	_, err := db.Exec("UPDATE messages SET is_sent = true WHERE id = $1", id)
+	return err
+}
+
+func updateMessageAsNotSent(db *sql.DB, id int) error {
+	log.Printf("Updating message %d as not sent", id)
+	_, err := db.Exec("UPDATE messages SET is_sent = false WHERE id = $1", id)
+	return err
 }
--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/lib/pq v1.10.7
 	github.com/streadway/amqp v1.0.0
 )
+
+require github.com/itimofeev/go-saga v0.1.0 // indirect
