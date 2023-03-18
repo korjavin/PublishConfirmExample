9f6354a8dcea7db8964441bccb424793290d0f38
Korzhavin Ivan <ikorjavin@3pm.ai>
2023-03-18 19:17:17 +0300

    add tx

--- cmd/publisher/main.go
+++ cmd/publisher/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/lib/pq"
 	"github.com/streadway/amqp"
@@ -61,21 +62,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Set publisher confirms on the channel
-	confirmsChan := make(chan amqp.Confirmation, 10)
-	// Enable publish confirmations
-	if err := ch.Confirm(false); err != nil {
-		close(confirmsChan)
-		fmt.Printf("Channel could not be put into confirm mode: %s\n", err)
-	}
-	confirms := ch.NotifyPublish(confirmsChan)
-	// defer confirmCleanup(ch, confirms)
-
 	// Loop through the result set and publish each id to the RabbitMQ queue
 	for rows.Next() {
 		var id int
 		if err := rows.Scan(&id); err != nil {
 			log.Fatal(err)
+		}
+
+		err := ch.Tx()
+		if err != nil {
+			log.Fatalf("Failed to begin a transaction: %v", err)
 		}
 
 		// Publish the message to the RabbitMQ queue
@@ -94,33 +90,21 @@ func main() {
 			log.Fatal(err)
 		}
 
-		// Wait for confirmation from the broker
-		log.Printf("Waiting for confirmation...")
-		confirm := <-confirms
-		if !confirm.Ack {
-			log.Fatalf("Failed to receive confirmation for message ID %d", id)
+		if id > 2 {
+			log.Printf("I will sleep for 30 sec, please find a way to kill postgresql to see if messages to rmq will be aborted as well")
+			time.Sleep(30 * time.Second)
 		}
 
 		// Set the "is_sent" flag to true for the current id in the database
 		_, err = db.Exec("UPDATE messages SET is_sent = true WHERE id = $1", id)
 		if err != nil {
+			ch.TxRollback()
 			log.Fatal(err)
 		}
+		ch.TxCommit()
 	}
 
 	if err := rows.Err(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-// Cleanup function to wait for confirmations before closing the channel
-func confirmCleanup(ch *amqp.Channel, confirms <-chan amqp.Confirmation) {
-	log.Printf("Waiting for publisher confirms...")
-	for confirm := range confirms {
-		if !confirm.Ack {
-			log.Fatalf("Error delivering message: %v", confirm)
-		}
-	}
-	log.Printf("All messages delivered successfully")
-	ch.Close()
 }
